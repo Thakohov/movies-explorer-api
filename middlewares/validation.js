3f36d92638dc67c4d4d7ed8f6c254a84406bf1cf
Эльдар <ethakohov@yandex.ru>
2024-01-11 12:24:16 +0300

    Первая версия кода для бэкенда

---
+++ middlewares/validation.js
@@ -0,0 +1,57 @@
+const { celebrate, Joi } = require('celebrate');
+const validator = require('validator');
+
+const isURL = (value, error) => (validator.isURL(value) ? value : error.message('Некорректная ссылка'));
+
+const validateCreateUser = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateLoginUser = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const validateUserInfo = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email(),
+  }),
+});
+
+// Поправить URL Валидацию у validateMovie
+const validateMovie = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().custom(isURL),
+    trailerLink: Joi.string().required().custom(isURL),
+    thumbnail: Joi.string().required().custom(isURL),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+const validateMovieId = celebrate({
+  params: Joi.object().keys({
+    _id: Joi.string().required().hex().length(24),
+  }),
+});
+
+module.exports = {
+  validateCreateUser,
+  validateLoginUser,
+  validateUserInfo,
+  validateMovie,
+  validateMovieId,
+};
